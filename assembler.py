fbd64c7c3f25f65872dc9fe236e2ac22e5483d77
Shashidhar Ravishankar <shashidhar.r.shankar@gmail.com>
2016-02-29 02:25:44 -0500

    subsampler modified

--- assembler.py
+++ assembler.py
@@ -119,6 +119,44 @@ def kmerOpt(fastq, outdir, krange=[35,45,55,65,75], size=10000000, readlen=0):
     results = pool.map(runKan, zip(repeat(read1), repeat(read2), repeat(outdir), krange))
     return
 
+def percAnalysis(arguments):
+    sam_size = arguments[0]
+    read1 = arguments[1]
+    read2 = arguments[2]
+    outdir = arguments[3]
+    ksize = arguments[4]
+    outdir = '{0}/subsample{1}'.format(outdir, sam_size)
+    if !os.path.exists(outdir):
+        os.mkdir(outdir)
+    spade_path = '/projects/home/sravishankar9/tools/SPAdes-3.7.0-Linux/bin/spades.py'
+    read1, read2 = subSample([read1, read2], outdir, size=sam_size)
+    spade_cmd = ['python', spade_path, '--careful', '-k', ksize, '--pe1-1', read1,
+            '--pe1-2', read2, '-o', outdir]
+    run_spades = subprocess.Popen(spade_cmd, shell=False)
+    run_spades.wait()
+    run_spades = None
+    return
+
+def multiAssemble(fastq, outdir):
+    outdir = os.path.abspath(outdir)
+    read1 = fastq[0]
+    read2 = fastq[1]
+    read_handle = SeqIO.parse(open(read1), 'fastq')
+    num_reads = 0
+    if !os.path.exists(outdir):
+        os.mkdir(outdir)
+    for line in read_handle:
+        num_reads += 1
+    size_range = [int(0.1*num_reads), int(0.25*num_reads), int(0.5*num_reads),
+                int(0.75*num_reads), int(0.90*num_reads), int(num_reads)]
+    ksize = '27,49,71,93,115,127'
+    pool = Pool(processes=1)
+    subsample = pool.map(percAnalysis, zip(size_range, repeat(read1),
+            repeat(read2), repeat(outdir), repeat(ksize)))
+    return
+
+
+
 
 if __name__ == '__main__':
     pbrazi =  argparse.ArgumentParser(prog='assembler')
@@ -133,13 +171,15 @@ if __name__ == '__main__':
     pbrazi.add_argument('-l', '--readlen', type=int, dest='readlen',
         default=0, help='Minimum read length for each read pair.')
     pbrazi.add_argument('-m', '--mode', type=str, dest='mode',
-        default='kan', choices=['sga','kan'], help='Mode of optimization.')
+        default='kan', choices=['sga','kan','spa'], help='Mode of optimization.')
     pbrazi.add_argument('--force', action='store_true', help='Force overwrite of results.')
     pbrazi.add_argument('-v', '--version', action='version', version='%(prog)s 0.9.0')
     opts = pbrazi.parse_args()
     if opts.mode == 'sga':
         metrics = sgaCorrect(opts.fastq, opts.outdir, opts.force, opts.krange,
                             opts.samplesize, opts.readlen)
+    elif opts.modde = 'spa':
+        multiAssemble(opts.fastq, opts.outdir)
     else:
         metrics = kmerOpt(opts.fastq, opts.outdir, opts.krange, opts.samplesize,
                         opts.readlen)
