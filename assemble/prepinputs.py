c0a21c34dce40d8b66ac79a075822a7f0cfd3c96
Shashidhar Ravishankar <shashidhar.r.shankar@gmail.com>
2017-03-12 15:51:40 -0400

    Prepinput code structure changed
    Prepinput has been moved to a class

--- assemble/prepinputs.py
+++ assemble/prepinputs.py
@@ -2,9 +2,11 @@ import os
 import sys
 import re
 import glob
+import logging
 from collections import namedtuple
 from assemble.readers import Fastq
 
+logger = logging.getLogger('Assembler')
 
 class Identifier:
 
@@ -13,6 +15,7 @@ class Identifier:
 
 
     def isIlluminaOld(self):
+        #@HWUSI-EAS100R:6:73:941:1973#0/1
         header_regex = re.compile('@\w+-?\w+:\d+:\d+:\d+:\d+#\d*')
         match = re.fullmatch(header_regex, self.rec.header)
         if match != None:
@@ -76,70 +79,80 @@ class Metrics:
         avg_length = total_length/float(total_reads)
         return(avg_length)
 
-def main(path):
-    path = os.path.abspath(path)
-    print(path)
-    files = glob.glob('{0}/*.fastq*'.format(path))
-    experiment = dict()
-    for fastq in files:
-        reader = Fastq(fastq, './', 'phred33')
-        Sample = namedtuple('Sample', ['sample', 'library', 'files', 'prep', 'paired'])
-        rec = next(reader.read())
-        identifier = Identifier(rec)    
-        metric = Metrics(fastq)
-        isIllOld =  identifier.isIlluminaOld()
-        isIllNew =  identifier.isIlluminaNew()
-        isSraOld = identifier.isSraOld()
-        isSraNew = identifier.isSraNew()
-        isPac = identifier.isPacbio()
-        seqType = ''
-        libType = ''
-        if isIllOld:
-            paired_regex = re.compile('@\w+-?\w+:\d+:\d+:\d+:\d+#\d')
-            lib = re.findall(paired_regex, rec.header)[0]
-            paired = False
-            seqType = 'Illumina'
-            if metric.avgReadLen():
-                libType = 'Short'
-        elif isIllNew:
-            paired_regex = re.compile('@\w+-?\w+:\d+:\w+-?\w+:\d+:\d+:\d+:\d+\s')
-            lib = re.findall(paired_regex, rec.header)[0]
-            paired = False
-            seqType = 'Illumina'
-            if metric.avgReadLen():
-                libType = 'Short'
-        elif isSraOld:
-            paired_regex = re.compile('@\w+\.?\w? \w+-\w+:\d+:\w+:\d+:\d+:\d+:\d+ length=\d+')
-            lib = re.findall(paired_regex, rec.header)[0]
-            paired = False
-            seqType = 'Illumina'
-            if metric.avgReadLen():
-                libType = 'Short'
-        elif isSraNew:
-            paired_regex = re.compile('@\w+\.?\w? \w+-\w+:\d+:\w+:\d+:\d+:\d+:\d+ length=\d+')
-            lib = re.findall(paired_regex, rec.header)[0]
-            paired = False
-            seqType = 'Illumina'
-            if metric.avgReadLen():
-                libType = 'Short'
-        elif isPac:
-            lib_regex = re.compile('@\w+_\d+_\d+_\w+')
-            lib = re.findall(lib_regex, rec.header)[0]
-            paired = False
-            seqType = 'Pacbio'
-            if metric.avgReadLen():
-                libType = 'Long'
-        try:
-            paired = True
-            experiment[lib] = Sample(lib, seqType, [experiment[lib].files[0], fastq], libType, paired)
-        except KeyError:            
-            experiment[lib] = Sample(lib, seqType, [fastq], libType, paired)
+class Prepper:
 
-    print(experiment)
-    return(experiment)
+    def __init__(self, input_path):
+        self.input_path = os.path.abspath(input_path)
+
+    def prepInputs(path):
+        files = glob.glob('{0}/*.fastq*'.format(self.input_path))
+        experiment = dict()
+        for fastq in files:
+            reader = Fastq(fastq, './', 'phred33')
+            Sample = namedtuple('Sample', ['sample', 'library', 'files', 'prep', 'paired'])
+            rec = next(reader.read())
+            identifier = Identifier(rec)    
+            metric = Metrics(fastq)
+            isIllOld =  identifier.isIlluminaOld()
+            isIllNew =  identifier.isIlluminaNew()
+            isSraOld = identifier.isSraOld()
+            isSraNew = identifier.isSraNew()
+            isPac = identifier.isPacbio()
+            seqType = ''
+            libType = ''
+            if isIllOld:
+                paired_regex = re.compile('@\w+-?\w+:\d+:\d+:\d+:\d+#\d')
+                lib = re.findall(paired_regex, rec.header)[0]
+                paired = False
+                seqType = 'Illumina'
+                if metric.avgReadLen():
+                    libType = 'Short'
+            elif isIllNew:
+                paired_regex = re.compile('@\w+-?\w+:\d+:\w+-?\w+:\d+:\d+:\d+:\d+\s')
+                lib = re.findall(paired_regex, rec.header)[0]
+                paired = False
+                seqType = 'Illumina'
+                if metric.avgReadLen():
+                    libType = 'Short'
+            elif isSraOld:
+                paired_regex = re.compile('@\w+\.?\w? \w+-\w+:\d+:\w+:\d+:\d+:\d+:\d+ length=\d+')
+                lib = re.findall(paired_regex, rec.header)[0]
+                paired = False
+                seqType = 'Illumina'
+                if metric.avgReadLen():
+                    libType = 'Short'
+            elif isSraNew:
+                paired_regex = re.compile('@\w+\.?\w? \w+-\w+:\d+:\w+:\d+:\d+:\d+:\d+ length=\d+')
+                lib = re.findall(paired_regex, rec.header)[0]
+                paired = False
+                seqType = 'Illumina'
+                if metric.avgReadLen():
+                    libType = 'Short'
+            elif isPac:
+                lib_regex = re.compile('@\w+_\d+_\d+_\w+')
+                lib = re.findall(lib_regex, rec.header)[0]
+                paired = False
+                seqType = 'Pacbio'
+                if metric.avgReadLen():
+                    libType = 'Long'
+            else:
+                logger.warning('Read from {0} with header : {1} does not follow any defined fastq header format.Please correct it'.format(fastq, rec_header))                    
+
+            try:
+                paired = True
+                experiment[lib] = Sample(lib, seqType, [experiment[lib].files[0], fastq], libType, paired)
+            except KeyError:            
+                experiment[lib] = Sample(lib, seqType, [fastq], libType, paired)
+
+        logger.info("The following libraries were detected in the given folder : {0}".format(self.input_path))
+        for files in experiment:
+            logger.info("Library: {0} ; Sequence type: {1} ; Files: {2} ; Library type: {3} ; Paired: {4}")
+
+        return(experiment)            
 
 if __name__ == '__main__':
     path = os.path.abspath(sys.argv[1])
-    main(path)
+    prepper = Fastq(path)
+    experiment = prepper.prepInputs()    
 
     
